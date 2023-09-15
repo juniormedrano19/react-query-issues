21305715d45be2b428f76a5044985a68be0a4a86
Junior Medrano <jmedrano@suizalab.com>
2023-09-15 18:38:56 -0500

    feat: Added new functions related to labels in facebook

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,5 +1,28 @@
+import { useQuery } from "@tanstack/react-query"
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
 
 export const LabelPicker = () => {
+
+  const getLabels=async(): Promise<Label[]>=>{
+    const {data}=await githubApi.get<Label[]>('/labels')
+ /*    const res= await fetch('https://api.github.com/repos/facebook/react/labels'); */
+ /*    const data= await res.json(); */
+ /*    console.log(data); */
+ return data;
+  }
+
+  //patrón adaptador con custom hooks
+  const labelsQuery=useQuery(
+    //espacio en caché que maneja 
+    ['labels'],
+    //function para traer esos labels
+    getLabels,{
+      refetchOnWindowFocus:false, 
+    }
+    //si quieres que refresque siempre tu data
+  )
+
   return (
     <div>
         <span 
